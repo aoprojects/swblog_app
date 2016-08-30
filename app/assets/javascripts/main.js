f8b01050e7c1af5af8ad673083ccbca4e80235de
Alison Ostendorf <aostendorf@gmail.com>
2016-08-30 10:40:35 -0400

    remove hard coded active class from nav, and write jquery to highlight active nav elements

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	console.log("ready");
+	$(".jobs_index");
+		$("#careers_nav_link").removeClass("active");
+		$("#blog_nav_link").addClass(".active");
+	$(".articles_index") || $(".articles_show");
+		$("#careers_nav_link").addClass(".active");
+		$("#blog_nav_link").removeClass(".active");
+});
