748cb4e4e08d19fb2dc94791e79a594244d782d4
Alison Ostendorf <aostendorf@gmail.com>
2016-08-30 11:01:05 -0400

    remove update and other actions from articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,9 @@
 class ArticlesController < ApplicationController
 
-
 	def index
 		@articles = Article.all
 	end
 
 	def show
 	end
-
-	def new
-	end
-
-	def create
-	end
-
-	def destroy
-	end
-
 end
