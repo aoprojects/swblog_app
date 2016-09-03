3c8eaf5de1a7900d401962737d5e3e11b6ade5e5
Alison Ostendorf <aostendorf@gmail.com>
2016-09-03 09:47:01 -0400

    make active nav link background, via css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-	console.log("ready");
-	$(".jobs_index");
-		$("#careers_nav_link").removeClass("active");
-		$("#blog_nav_link").addClass(".active");
-	$(".articles_index") || $(".articles_show");
-		$("#careers_nav_link").addClass(".active");
-		$("#blog_nav_link").removeClass(".active");
+	console.log("jq is working");
+	
 });
