9967db7c7532acef30c660d2449b8911d7968550
Alison Ostendorf <aostendorf@gmail.com>
2016-09-03 10:43:06 -0400

    add to footer that this site is a beta

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,98 +6,98 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Job.create(title: "Executive Assistant", location: "Rancho Palos Verdes, Los Angeles County, California", description: "<p>
-    Socialwise Partners Inc. seeks a full-time, experienced executive assistant to work closely with the CEO to help manage the rapid growth of our new
-    company.
-</p>
-<p>
-    Socialwise is a social enterprise that distributes office and business products on a B2B basis, with sales operations launching in New York and California.
-    We are a B Corporation and, as part of our social enterprise mission, we donate all net profit to charitable causes.
-</p>
+# Job.create(title: "Executive Assistant", location: "Rancho Palos Verdes, Los Angeles County, California", description: "<p>
+#     Socialwise Partners Inc. seeks a full-time, experienced executive assistant to work closely with the CEO to help manage the rapid growth of our new
+#     company.
+# </p>
+# <p>
+#     Socialwise is a social enterprise that distributes office and business products on a B2B basis, with sales operations launching in New York and California.
+#     We are a B Corporation and, as part of our social enterprise mission, we donate all net profit to charitable causes.
+# </p>
 
-<p>
-    At Socialwise, we seek to develop a family and people-first environment. We value people for their rich histories and perspectives rather than things like
-    certifications and degrees, former employment brands, or buzzwords on resumes. We love friendly candidates and we believe that our employees are our best
-    resource. A startup with a great idea can only stay great by hiring and keeping the best people possible.
-</p>
+# <p>
+#     At Socialwise, we seek to develop a family and people-first environment. We value people for their rich histories and perspectives rather than things like
+#     certifications and degrees, former employment brands, or buzzwords on resumes. We love friendly candidates and we believe that our employees are our best
+#     resource. A startup with a great idea can only stay great by hiring and keeping the best people possible.
+# </p>
 
-<p>
-    If you are fanatical about your piece in making the world a better place, if you live well by schedules and budgets, and if you can also maintain a
-    positive and friendly attitude in the midst of startup evolution chaos, we want to speak with you.
-</p>
+# <p>
+#     If you are fanatical about your piece in making the world a better place, if you live well by schedules and budgets, and if you can also maintain a
+#     positive and friendly attitude in the midst of startup evolution chaos, we want to speak with you.
+# </p>
 
-<p>
-    We are seeking a full-time, experienced executive assistant to work closely with the CEO to help manage the rapid growth of our new company in Palos
-    Verdes, California. Candidates should have 2-4 years of experience in administrative tasks, and/or advanced customer service roles. The right candidate
-    will be highly organized, tech-savvy, flexible, a quick learner who reliably completes all assignments, and proactive with a positive attitude that can be
-    heard over the phone. Ability to work effectively in all aspects of operating a business is a plus. An entrepreneurial attitude and focus is preferred.
-</p>
+# <p>
+#     We are seeking a full-time, experienced executive assistant to work closely with the CEO to help manage the rapid growth of our new company in Palos
+#     Verdes, California. Candidates should have 2-4 years of experience in administrative tasks, and/or advanced customer service roles. The right candidate
+#     will be highly organized, tech-savvy, flexible, a quick learner who reliably completes all assignments, and proactive with a positive attitude that can be
+#     heard over the phone. Ability to work effectively in all aspects of operating a business is a plus. An entrepreneurial attitude and focus is preferred.
+# </p>
 
-<p>
-    This full-time position offers long-term growth potential as we expand nationally over the coming years. Work hours are 8am-5pm, Monday through Friday,
-    with some flexibility available. The salary for this position is $3200 per month, negotiable based on experience. Health, dental, and vision insurance are
-    available.
-</p>
+# <p>
+#     This full-time position offers long-term growth potential as we expand nationally over the coming years. Work hours are 8am-5pm, Monday through Friday,
+#     with some flexibility available. The salary for this position is $3200 per month, negotiable based on experience. Health, dental, and vision insurance are
+#     available.
+# </p>
 
-<p>
-    Socialwise is an equal opportunity employer. See us at www.so-wise.com.
-</p>
+# <p>
+#     Socialwise is an equal opportunity employer. See us at www.so-wise.com.
+# </p>
 
-<p>
-    There are two kinds of employees: 
-    <ol>
-    <li> Those who finish a task and wait to be assigned another </li>
-    <li> Those who finish a task and ask, “What can I do next?”</li>
-    </ol>
-    If you are in Group 2, please apply.
-</p>
+# <p>
+#     There are two kinds of employees: 
+#     <ol>
+#     <li> Those who finish a task and wait to be assigned another </li>
+#     <li> Those who finish a task and ask, “What can I do next?”</li>
+#     </ol>
+#     If you are in Group 2, please apply.
+# </p>
 
-<p>
-    We promise to acknowledge every inquiry with a personal response, because we have no robots currently working in HR.
-</p>")
+# <p>
+#     We promise to acknowledge every inquiry with a personal response, because we have no robots currently working in HR.
+# </p>")
 
-Job.create(title: "Outside Professional Sales Representative", location: "New York, NY", description: "<p>Socialwise Partners Inc. seeks a full-time, experienced outside sales rep to work in NYC to fuel the rapid growth of our new company.</p>
+# Job.create(title: "Outside Professional Sales Representative", location: "New York, NY", description: "<p>Socialwise Partners Inc. seeks a full-time, experienced outside sales rep to work in NYC to fuel the rapid growth of our new company.</p>
 
-<p>Socialwise is a social enterprise that distributes office and business products on a B2B basis, with sales operations currently in New York and California.  We are a registered Benefit Corporation and, as part of our social enterprise mission, we donate all of our net profits to charitable causes.</p>
+# <p>Socialwise is a social enterprise that distributes office and business products on a B2B basis, with sales operations currently in New York and California.  We are a registered Benefit Corporation and, as part of our social enterprise mission, we donate all of our net profits to charitable causes.</p>
 
-<p>At Socialwise, we seek to develop a family and people-first environment.  We value people for their rich histories and perspectives rather than things like certifications and degrees, former employment brands, or buzzwords on resumes.  We love friendly candidates and we believe that our employees are our best resource.  A startup with a great idea can only stay great by hiring and keeping the best people possible.</p>
+# <p>At Socialwise, we seek to develop a family and people-first environment.  We value people for their rich histories and perspectives rather than things like certifications and degrees, former employment brands, or buzzwords on resumes.  We love friendly candidates and we believe that our employees are our best resource.  A startup with a great idea can only stay great by hiring and keeping the best people possible.</p>
 
-<p>We want to speak with you if:
-<ul>
-<li>you are fanatical about your piece in making the world a better place</li>
-<li>you live well by schedules and budgets</li>
-<li>you can talk with strangers like they are your friends</li>
-<li>you treat the janitor as well as the CEO</li>
-<li>you can AND will close new business and meet or exceed established sales and calls targets</li>
-<li>you can maintain a positive and success-oriented attitude in the midst of startup evolution chaos</li>
-</ul>
-</p>
+# <p>We want to speak with you if:
+# <ul>
+# <li>you are fanatical about your piece in making the world a better place</li>
+# <li>you live well by schedules and budgets</li>
+# <li>you can talk with strangers like they are your friends</li>
+# <li>you treat the janitor as well as the CEO</li>
+# <li>you can AND will close new business and meet or exceed established sales and calls targets</li>
+# <li>you can maintain a positive and success-oriented attitude in the midst of startup evolution chaos</li>
+# </ul>
+# </p>
 
-<p>Preferred candidates will:
+# <p>Preferred candidates will:
 
-<ul>
-<li>have 2-4 years of experience in B2B outside sales –OR- compelling reason for us to be convinced that you have the ability to succeed in B2B sales calling on companies of all sizes</li>
-<li>have the ability to generate new business through networking, cold calling, prospecting, etc.</li>
-<li>have superior customer service and account management skills</li>
-<li>be highly organized, tech-savvy, flexible, and a quick learner who reliably completes all assignments</li>
-<li>be proactive with a positive attitude that can be heard over the phone as much as in person</li>
-<li>be given preference for entrepreneurial experience and focus</li>
-</ul>
-</p>
+# <ul>
+# <li>have 2-4 years of experience in B2B outside sales –OR- compelling reason for us to be convinced that you have the ability to succeed in B2B sales calling on companies of all sizes</li>
+# <li>have the ability to generate new business through networking, cold calling, prospecting, etc.</li>
+# <li>have superior customer service and account management skills</li>
+# <li>be highly organized, tech-savvy, flexible, and a quick learner who reliably completes all assignments</li>
+# <li>be proactive with a positive attitude that can be heard over the phone as much as in person</li>
+# <li>be given preference for entrepreneurial experience and focus</li>
+# </ul>
+# </p>
 
-<p>This full-time position offers long-term growth and management potential as we expand nationally over the coming years. Work hours are 9am-6pm, Monday through Friday, although achievement of sales goals will open the door to significant flexibility.  The starting salary for this position is $4,000 per month, negotiable based on experience.  Commission potential exceeds $100,000 per year.  Health, dental, and vision insurance are available. </p>
- 
-<p>Socialwise is an equal opportunity employer. See us at <b>www.so-wise.com.</b></p>
- 
-<p>There are two kinds of employees:  
-<ol>
-<li>Those who finish a task and wait to be assigned another</li>
-<li>Those who finish a task and ask, “What can I do next?”</li>
-</ol>
-If you are in Group 2, please submit your resume.</p>
+# <p>This full-time position offers long-term growth and management potential as we expand nationally over the coming years. Work hours are 9am-6pm, Monday through Friday, although achievement of sales goals will open the door to significant flexibility.  The starting salary for this position is $4,000 per month, negotiable based on experience.  Commission potential exceeds $100,000 per year.  Health, dental, and vision insurance are available. </p>
+#  
+# <p>Socialwise is an equal opportunity employer. See us at <b>www.so-wise.com.</b></p>
+#  
+# <p>There are two kinds of employees:  
+# <ol>
+# <li>Those who finish a task and wait to be assigned another</li>
+# <li>Those who finish a task and ask, “What can I do next?”</li>
+# </ol>
+# If you are in Group 2, please submit your resume.</p>
 
-<p>We promise to acknowledge every inquiry with a personal response, because we have no robots currently working in HR.</p>
-")
+# <p>We promise to acknowledge every inquiry with a personal response, because we have no robots currently working in HR.</p>
+# ")
 
 Article.create(title: "Why a Green Business Still Prints Paper Fliers", date: "August 2016", body_text: "<div class="articles">
 
